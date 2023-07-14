9c62cb2af27415bebdc28828d3373cc3227e40ac
Amirable <125992348+Amirable@users.noreply.github.com>
2023-07-14 19:28:48 +0100

    done

--- ster.js
+++ ster.js
@@ -1,54 +1,51 @@
+// const firebaseConfig = {
+//    apiKey: "AIzaSyCUDTrmdO2nW_H2CTk8S1M1MvD6euJpPLg",
+//    authDomain: "register-7fc8f.firebaseapp.com",
+//    projectId: "register-7fc8f",
+//    storageBucket: "register-7fc8f.appspot.com",
+//    messagingSenderId: "1092275612543",
+//    appId: "1:1092275612543:web:69a412543579549c0cc84a"
+//   };
+
 const firebaseConfig = {
    apiKey: "AIzaSyCUDTrmdO2nW_H2CTk8S1M1MvD6euJpPLg",
    authDomain: "register-7fc8f.firebaseapp.com",
+   databaseURL: "https://register-7fc8f-default-rtdb.firebaseio.com",
    projectId: "register-7fc8f",
    storageBucket: "register-7fc8f.appspot.com",
    messagingSenderId: "1092275612543",
-   appId: "1:1092275612543:web:69a412543579549c0cc84a"
-  };
-  firebase.initializeApp(firebaseConfig);
+   appId: "1:1092275612543:web:b933924f73d7b12e0cc84a"
+ };
+  
+ firebase.initializeApp(firebaseConfig);
 
   const auth = firebase.auth();
+ 
 //   signup
-   const signupBtn = document.querySelector('#signup-btn');
-    signupBtn.addEventListener('click', e => {
-    e.preventDefault();
-
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log('User signed up!');
-    location.reload();
-    alert('user signup')
-  })
-   .catch((error) => {
-   var errorCode = error.code;
-   var errorMessage = error.message;
-   console.log(errorCode);
-   console.log(errorMessage);
-   alert(errorMessage)
-   });
-});
-
-
-// log in
-const loginBtn = document.querySelector('#login-btn');
-loginBtn.addEventListener('click', e => {
+let signUpButton = document.getElementById("signup-btn");
+signUpButton.addEventListener("click", (e) => {
    e.preventDefault();
+   console.log("clicked");
 
-   const email = document.querySelector('#email').value;
-   const password = document.querySelector('#password').value;
+   var email = document.getElementById("email");
+   var password = document.getElementById("password");
 
-   auth.signInWithEmailAndPassword(email, password)
-   .then(cred => {
-      console.log('logged in');
+   auth
+   .createUserWithEmailAndPassword(email.value, password.value)
+   .then((userCredential) => {
       location.reload();
-      alert('logged in user')
+      alert("user signed up successfully")
+
+
+      var user = userCredential.user;
+      console.log("user", user.email);
    })
-   .catch(error => {
-      console.log(error.message);
+
+   .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("error code", errorCode);
+      console.log("error code", errorMessage);
       alert(errorMessage)
    })
 });
@@ -56,25 +53,106 @@ loginBtn.addEventListener('click', e => {
 
 
 
-// logout
-const logoutBtn = document.querySelector('#logout-btn');
-logoutBtn.addEventListener('click', e => {
-   e.preventDefault();
-   auth.signOut();
-   console.log('User signed out!');
-   alert('logged out')
-})
 
 
-// keeping track of auth state
-auth.onAuthStateChanged(user => {
-   if (user) {
-      console.log(user.email + "is logged in!");
-   } else {
-      console.log('User is logged out!');
-      alert('user loggedout!')
-   }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   signup
+//    const signupBtn = document.getElementById('signup-btn');
+//     signupBtn.addEventListener('click', e => {
+//     e.preventDefault();
+
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
+
+//     auth.createUserWithEmailAndPassword(email, password).then(Cred => {
+//     console.log('User signed up!');
+//     location.reload();
+//     alert('user signup')
+//   })
+//    .catch((error) => {
+//    var errorCode = error.code;
+//    var errorMessage = error.message;
+//    console.log(errorCode);
+//    console.log(errorMessage);
+//    alert(errorMessage)
+//    });
+// });
+
+
+// // log in
+// const loginBtn = document.getElementById('login-btn');
+// loginBtn.addEventListener('click', e => {
+//    e.preventDefault();
+
+//    const email = document.getElementById('email').value;
+//    const password = document.getElementById('password').value;
+
+//    auth.signInWithEmailAndPassword(email, password)
+//    .then(cred => {
+//       console.log('logged in');
+//       location.reload();
+//       alert('logged in user')
+//    })
+//    .catch(error => {
+//       console.log(error.message);
+//       alert(errorMessage)
+//    })
+// });
+
+
+
+
+// // logout
+// const logoutBtn = document.getElementById('logout-btn');
+// logoutBtn.addEventListener('click', e => {
+//    e.preventDefault();
+//    auth.signOut();
+//    console.log('User signed out!');
+//    alert('logged out')
+// })
+
+
+// // keeping track of auth state
+// auth.onAuthStateChanged(user => {
+//    if (user) {
+//       console.log(user.email + "is logged in!");
+//    } else {
+//       console.log('User is logged out!');
+//       alert('user loggedout!');
+//       window.location = "index.html";
+//    }
+   
+   
+// });
 
 
 
