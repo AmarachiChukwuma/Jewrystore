800f4542891198efa3adb9561d10ef3348007596
Amirable <125992348+Amirable@users.noreply.github.com>
2023-07-14 13:13:56 +0100

    done

--- ster.js
+++ ster.js
@@ -36,25 +36,18 @@ const firebaseConfig = {
 // log in
 const loginBtn = document.querySelector('#login-btn');
 loginBtn.addEventListener('click', e => {
-e.preventDefault();
+   e.preventDefault();
 
-const email = document.querySelector('#email').value;
-const password = document.querySelector('#password').value;
+   const email = document.querySelector('#email').value;
+   const password = document.querySelector('#password').value;
 
-   auth.loginWithEmailAndPassword(email, password)
-  .then(cred => {
-    console.log('logged in user!');
-    location.reload();
-    alert('logged in user!')
-  })
-  .catch(error => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-    alert(errorMessage)
-  });
-});
+   auth.signInWithEmailAndPassword(email, password)
+   .then(cred => {
+      console.log('logged in');
+      location.reload();
+      alert('logged in user')
+   })
+})
 
 
 
