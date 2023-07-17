353a79b746500a9b35870c4333db149734c25c0f
Amirable <125992348+Amirable@users.noreply.github.com>
2023-07-17 09:00:11 +0100

    done

--- javascript/ster.js
+++ javascript/ster.js
@@ -1,11 +1,3 @@
-// const firebaseConfig = {
-//    apiKey: "AIzaSyCUDTrmdO2nW_H2CTk8S1M1MvD6euJpPLg",
-//    authDomain: "register-7fc8f.firebaseapp.com",
-//    projectId: "register-7fc8f",
-//    storageBucket: "register-7fc8f.appspot.com",
-//    messagingSenderId: "1092275612543",
-//    appId: "1:1092275612543:web:69a412543579549c0cc84a"
-//   };
 
 const firebaseConfig = {
    apiKey: "AIzaSyCUDTrmdO2nW_H2CTk8S1M1MvD6euJpPLg",
@@ -22,76 +14,6 @@ const firebaseConfig = {
   const auth = firebase.auth();
  
 //   signup
-// let signupBtn = document.getElementById("signup-btn");
-// signupButton.addEventListener("click", (e) => {
-//    e.preventDefault();
-//    console.log("clicked");
-
-//    var email = document.getElementById("email");
-//    var password = document.getElementById("password");
-
-//    auth
-//    .createUserWithEmailAndPassword(email.value, password.value)
-//    .then((userCredential) => {
-//       location.reload();
-//       alert("user signed up successfully")
-
-
-//       var user = userCredential.user;
-//       console.log("user", user.email);
-//    })
-
-//    .catch((error) => {
-//       var errorCode = error.code;
-//       var errorMessage = error.message;
-//       console.log("error code", errorCode);
-//       console.log("error code", errorMessage);
-//       alert(errorMessage)
-//    })
-// });
-
-
-// // signin
-
-// let signInBtn = document.getElementById("signin-btn");
-// signInBtn.addEventListener("click", (e) => {
-//    e.preventDefault();
-//    console.log("clicked");
-
-//    var email = document.getElementById("signin-btn");
-//    var password = document.getElementById("password");
-
-//    auth
-//    .signInUserWithEmailAndPassword(email.value, password.value)
-//    .then((userCredential) => {
-//       var user = userCredential.user;
-//       console.log("user", user.email);
-//       alert("user logged in")
-//       window.location = "index.html";
-//    })
-
-//    .catch((error) => {
-//       var errorCode = error.code;
-//       var errorMessage = error.message;
-//       alert(errorMessage)
-//    })
-// });
-
-// keeping track of auth state
-// auth.onAuthStateChanged(user => {
-//    if (user) {
-//       console.log(user.email + "is logged in!");
-//    } else {
-//       console.log('User is looged out!');
-//       alert('user loggedout!');
-//    }
-// })
-
-
-
-
-
-//   signup
     const signUpBtn = document.getElementById('signup-btn');
     signUpBtn.addEventListener('click', e => {
     e.preventDefault();
@@ -100,12 +22,12 @@ const firebaseConfig = {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    auth.signInWithEmailAndPassword(email, password)
+    auth.createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
          var user = userCredential.user;
          console.log("user", user.email);
-         alert("user logged in")
-         window.location = "index.html";
+         alert("user signed Up")
+         // window.location = "index.html";
   })
    .catch((error) => {
    var errorCode = error.code;
@@ -118,18 +40,20 @@ const firebaseConfig = {
 
 
 // log in
-const signinbtn = document.getElementById("signin-btn");
-signinbtn.addEventListener('click', e => {
+const signInBtn = document.getElementById("signin-btn");
+signInBtn.addEventListener('click', e => {
    e.preventDefault();
 
    const email = document.getElementById('email').value;
    const password = document.getElementById('password').value;
 
    auth.signInWithEmailAndPassword(email, password)
-   .then(cred => {
-      console.log('logged in');
+   .then( (userCredential) => {
+      var user = userCredential.user;
+      console.log("user", user.email);
+      alert("user logged in")
       location.reload();
-      alert('logged in user')
+      window.location = "index.html";
    })
    .catch(error => {
       console.log(error.message);
