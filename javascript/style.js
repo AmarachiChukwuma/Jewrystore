030168562a5a5e23d6969fe237d2caf86286eb43
Amirable <125992348+Amirable@users.noreply.github.com>
2023-07-17 12:37:25 +0100

    done

--- javascript/style.js
+++ javascript/style.js
@@ -1,197 +1,195 @@
-// function show(id) {
-//     if(document.getElementById(id).style.display == 'none') {
-//         document.getElementById(id).style.display= 'block';
-//     } else{
-//         document.getElementById(id).style.display = 'none';
-//     }
-// };
+// // function show(id) {
+// //     if(document.getElementById(id).style.display == 'none') {
+// //         document.getElementById(id).style.display= 'block';
+// //     } else{
+// //         document.getElementById(id).style.display = 'none';
+// //     }
+// // };
 
-// // submit button
-// function Verify() {
-//     var userRef = "AmicAble";
-//     var passRef = "12ruthieAma";
+// // // submit button
+// // function Verify() {
+// //     var userRef = "AmicAble";
+// //     var passRef = "12ruthieAma";
 
-//     var user = document.getElementById("username").value;
-//     var pass = document.getElementById("password").value;
-//     if(user == userRef || pass == passRef) {
-//         alert("Loggedin")
-//         window.location(index.html)
-//     } else{
-//         alert("It's seem you make a mistake...")
-//     }
-// };
+// //     var user = document.getElementById("username").value;
+// //     var pass = document.getElementById("password").value;
+// //     if(user == userRef || pass == passRef) {
+// //         alert("Loggedin")
+// //         window.location(index.html)
+// //     } else{
+// //         alert("It's seem you make a mistake...")
+// //     }
+// // };
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCUDTrmdO2nW_H2CTk8S1M1MvD6euJpPLg",
-    authDomain: "register-7fc8f.firebaseapp.com",
-    databaseURL: "https://register-7fc8f-default-rtdb.firebaseio.com",
-    projectId: "register-7fc8f",
-    storageBucket: "register-7fc8f.appspot.com",
-    messagingSenderId: "1092275612543",
-    appId: "1:1092275612543:web:b933924f73d7b12e0cc84a"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCUDTrmdO2nW_H2CTk8S1M1MvD6euJpPLg",
+//     authDomain: "register-7fc8f.firebaseapp.com",
+//     databaseURL: "https://register-7fc8f-default-rtdb.firebaseio.com",
+//     projectId: "register-7fc8f",
+//     storageBucket: "register-7fc8f.appspot.com",
+//     messagingSenderId: "1092275612543",
+//     appId: "1:1092275612543:web:b933924f73d7b12e0cc84a"
+//   };
 
-    // Initialize Firebase   
-  firebase.initializeApp(firebaseConfig);
+//     // Initialize Firebase   
+//   firebase.initializeApp(firebaseConfig);
  
 
-    // invokes firebase authentication  
-   const auth = firebase.auth();
-   document.querySelector("#show-register").addEventListener("click", () => {
-    showRegistration();
-   });
+//     // invokes firebase authentication  
+//    const auth = firebase.auth();
+//    document.querySelector("#show-register").addEventListener("click", () => {
+//     showRegistration();
+//    });
 
-   const showRegistration = () => {
-    document.querySelector("#registration-page").classList.remove("hide");
-    document.querySelector("#login-page").classList.add("hide");
-    document.querySelector("#").classList.add("hide");
-   };
+//    const showRegistration = () => {
+//     document.querySelector("#registration-page").classList.remove("hide");
+//     document.querySelector("#login-page").classList.add("hide");
+//     document.querySelector("#").classList.add("hide");
+//    };
    
-   document.querySelector("#show-login").addEventListener("click", () => {
-    showLogin();
-   });
+//    document.querySelector("#show-login").addEventListener("click", () => {
+//     showLogin();
+//    });
 
-   const showLogin = () => {
-    document.querySelector("#registration-page").classList.add("hide");
-    document.querySelector("#login-page").classList.remove("hide");
-    document.querySelector("#login-page").classList.add("hide");
-   };
+//    const showLogin = () => {
+//     document.querySelector("#registration-page").classList.add("hide");
+//     document.querySelector("#login-page").classList.remove("hide");
+//     document.querySelector("#login-page").classList.add("hide");
+//    };
 
-   document.querySelector("#signout").addEventListener("click", () => {
-    signOut();
-   });
+//    document.querySelector("#signout").addEventListener("click", () => {
+//     signOut();
+//    });
 
-   const register = () => {
-    const email = document.querySelector("#registration-email").value;
-    const reemail = document.querySelector("#registration-reemail").value;
-    const password = document.querySelector("#registration-password").value;
+//    const register = () => {
+//     const email = document.querySelector("#registration-email").value;
+//     const reemail = document.querySelector("#registration-reemail").value;
+//     const password = document.querySelector("#registration-password").value;
 
-    if(email.trim() == ""){
-        alert("Enter Email");
-    } else if (password.trim().length < 7) {
-        alert("password must be at least 7 characters");
-    } else if (email != reemail) {
-        alert("emails do not match");
-    } else {
-        auth
-        .createUserWithEmailAndPassword(email, password)
-        .catch(function (error) {
-            // handle error here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            alert(errorMessage)
-        });
-    }
-   };
+//     if(email.trim() == ""){
+//         alert("Enter Email");
+//     } else if (password.trim().length < 7) {
+//         alert("password must be at least 7 characters");
+//     } else if (email != reemail) {
+//         alert("emails do not match");
+//     } else {
+//         auth
+//         .createUserWithEmailAndPassword(email, password)
+//         .catch(function (error) {
+//             // handle error here.
+//             var errorCode = error.code;
+//             var errorMessage = error.message;
+//             alert(errorMessage)
+//         });
+//     }
+//    };
 
-   document.querySelector("#register").addEventListener("click", () => {
-    register();
-   });
+//    document.querySelector("#register").addEventListener("click", () => {
+//     register();
+//    });
 
-//    register when you hit the enter key
-document
-.querySelector("#registration-password")
-.addEventListener("keyup", (e) => {
-    if (event.keyCode === 13) {
-        e.preventDefault();
+// //    register when you hit the enter key
+// document
+// .querySelector("#registration-password")
+// .addEventListener("keyup", (e) => {
+//     if (event.keyCode === 13) {
+//         e.preventDefault();
 
-        register();
-    }
-});
-
-
-
-const login = () => {
-    const email = document.querySelector("#login-email").value;
-    const password = document.querySelector("#login-password").value;
-
-    if(email.trim() == "") {
-        alert("Enter Email");
-    } else if (password.trim() == "") {
-        alert("Enter Password");
-    } else {
-        authenticate(email, password);
-    }
-};
-
-document.querySelector("#login").addEventListener("click", () => {
-    login();
-});
-
-// sign in when you hit enter
-document
-.querySelector("#login-password")
-.addEventListener("keyup", (e) => {
-    if (event.keyCode === 13) {
-        e.preventDefault();
-
-        login();
-    }
-});
-
-const authenticate = (email, password) => {
-    const auth = firebase.auth();
-    auth.signInWithEmailAndPassword(email, password);
-    firebase
-    .auth()
-    signInWithEmailAndPassword(email, password)
-    .catch(function (error) {
-        // handle errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
-
-    });
-};
-
-const showHomepage = () => {
-    document.querySelector("#registration-page").classList.add("hide");
-    document.querySelector("#login-page").classList.add("hide");
-    document.querySelector("#homepage").classList.remove("hide");
-};
-
-const signOut =() => {
-    firebase
-    .auth()
-    .signOut()
-    .then(function () {
-        location.reload();
-    })
-    .catch(function (error) {
-        alert("error signing out, check network conncetion");
-    });
-};
-
-auth.onAuthStateChanged((firebaseUser) => {
-    if (firebaseUser) {
-        showHomepage();
-    }
-});
-
-document
-.querySelector("#forgot-password")
-.addEventListener("click", () => {
-    const email = document.querySelector("#login-email").value;
-    if (email.trim() == "") {
-        alert("Enter Email");
-    } else {
-        fogotPassword(email);
-    }
-});
-
-const fogotPassword = (email) => {
-    auth 
-    .sendPasswordResetEmail(email)
-    .then(function () {
-    alert("email sent");
-    })
-    .catch(function (error) {
-        alert("invalid email or bad network")
-    });
-};
+//         register();
+//     }
+// });
 
 
+
+// const login = () => {
+//     const email = document.querySelector("#login-email").value;
+//     const password = document.querySelector("#login-password").value;
+
+//     if(email.trim() == "") {
+//         alert("Enter Email");
+//     } else if (password.trim() == "") {
+//         alert("Enter Password");
+//     } else {
+//         authenticate(email, password);
+//     }
+// };
+
+// document.querySelector("#login").addEventListener("click", () => {
+//     login();
+// });
+
+// // sign in when you hit enter
+// document
+// .querySelector("#login-password")
+// .addEventListener("keyup", (e) => {
+//     if (event.keyCode === 13) {
+//         e.preventDefault();
+
+//         login();
+//     }
+// });
+
+// const authenticate = (email, password) => {
+//     const auth = firebase.auth();
+//     auth.signInWithEmailAndPassword(email, password);
+//     firebase
+//     .auth()
+//     signInWithEmailAndPassword(email, password)
+//     .catch(function (error) {
+//         // handle errors here.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//         alert(errorMessage);
+
+//     });
+// };
+
+// const showHomepage = () => {
+//     document.querySelector("#registration-page").classList.add("hide");
+//     document.querySelector("#login-page").classList.add("hide");
+//     document.querySelector("#homepage").classList.remove("hide");
+// };
+
+// const signOut =() => {
+//     firebase
+//     .auth()
+//     .signOut()
+//     .then(function () {
+//         location.reload();
+//     })
+//     .catch(function (error) {
+//         alert("error signing out, check network conncetion");
+//     });
+// };
+
+// auth.onAuthStateChanged((firebaseUser) => {
+//     if (firebaseUser) {
+//         showHomepage();
+//     }
+// });
+
+// document
+// .querySelector("#forgot-password")
+// .addEventListener("click", () => {
+//     const email = document.querySelector("#login-email").value;
+//     if (email.trim() == "") {
+//         alert("Enter Email");
+//     } else {
+//         fogotPassword(email);
+//     }
+// });
+
+// const fogotPassword = (email) => {
+//     auth 
+//     .sendPasswordResetEmail(email)
+//     .then(function () {
+//     alert("email sent");
+//     })
+//     .catch(function (error) {
+//         alert("invalid email or bad network")
+//     });
+// };
 
 
 
@@ -209,38 +207,40 @@ const fogotPassword = (email) => {
 
 
 
-// cart
-let cartIcon = document.querySelector('#cart-icon');
-let cart = document.querySelector('.cart');
-let closeCart = document.querySelector('#close-cart');
 
-// opening cart
-cartIcon.onclick = () =>{
-    cart.classList.add("active");
-};
 
-// closing cart
-closeCart.onclick = () =>{
-    cart.classList.remove("active");
-};
+// // cart
+// let cartIcon = document.querySelector('#cart-icon');
+// let cart = document.querySelector('.cart');
+// let closeCart = document.querySelector('#close-cart');
 
-// cart working js
-if(document.readyState == 'loading'){
-    document.addEventListener('DOMContent', ready);
-} else{
-    ready();
-}
+// // opening cart
+// cartIcon.onclick = () =>{
+//     cart.classList.add("active");
+// };
 
-// making fuction
-function ready() {
-    // remove items from cart
-    var removeCartButtons = document.getElementsByClassName('cart-remove')
-    console.log(removeCartButtons)
-    for(var i = 0; i < removeCartButtons.length; i++){
-        var button = removeCartButtons[i]
-        button.addEventListener('click' removeCartItem)
-    }
-}
+// // closing cart
+// closeCart.onclick = () =>{
+//     cart.classList.remove("active");
+// };
+
+// // cart working js
+// if(document.readyState == 'loading'){
+//     document.addEventListener('DOMContent', ready);
+// } else{
+//     ready();
+// }
+
+// // making fuction
+// function ready() {
+//     // remove items from cart
+//     var removeCartButtons = document.getElementsByClassName('cart-remove')
+//     console.log(removeCartButtons)
+//     for(var i = 0; i < removeCartButtons.length; i++){
+//         var button = removeCartButtons[i]
+//         button.addEventListener('click' removeCartItem)
+//     }
+// }
 
 
 
