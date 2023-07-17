039c43d71aa4c0e4524f4caf5fc078b169c20924
Amirable <125992348+Amirable@users.noreply.github.com>
2023-07-17 12:49:11 +0100

    done

--- javascript/other.js
+++ javascript/other.js
@@ -15,3 +15,29 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
 }
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUDTrmdO2nW_H2CTk8S1M1MvD6euJpPLg",
+  authDomain: "register-7fc8f.firebaseapp.com",
+  databaseURL: "https://register-7fc8f-default-rtdb.firebaseio.com",
+  projectId: "register-7fc8f",
+  storageBucket: "register-7fc8f.appspot.com",
+  messagingSenderId: "1092275612543",
+  appId: "1:1092275612543:web:b933924f73d7b12e0cc84a"
+};
+ 
+firebase.initializeApp(firebaseConfig);
+
+ const auth = firebase.auth();
+
+
+// logout
+const logoutBtn = document.getElementById('logout-btn');
+logoutBtn.addEventListener('click', e => {
+ e.preventDefault();
+ auth.signOut();
+ console.log('User signed out!');
+ alert('logged out');
+ window.location = "login.html"
+})
